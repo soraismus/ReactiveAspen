02cc7fd4a540feaf982157adef72a93407857468
Soraismus <tertius_lupus@yahoo.com>
2014-12-11 19:29:28 -0500

    Recompile after adding and exporting the fn 'connectBus'.

--- js/adapter/port-registrar.js
+++ js/adapter/port-registrar.js
@@ -1,4 +1,4 @@
-var addComponent, connectPort, connectPortComponent, createEventStreamBus, getComponent, getPortComponent, keypaths, portExtension, ports, register, _ref;
+var addComponent, busExt, connectBus, connectPort, connectPortComponent, createEventStreamBus, getComponent, getPortComponent, keypaths, portExt, ports, register, _ref, _ref1;
 
 _ref = require('../utilities.js'), addComponent = _ref.addComponent, getComponent = _ref.getComponent;
 
@@ -8,7 +8,9 @@ keypaths = {};
 
 ports = {};
 
-portExtension = '.port';
+busExt = '.bus';
+
+portExt = '.port';
 
 connectPortComponent = function(extension) {
   return function(keypath) {
@@ -37,9 +39,10 @@ register = function(keypath) {
   return keypaths[keypath] = true;
 };
 
-connectPort = connectPortComponent(portExtension);
+_ref1 = [busExt, portExt].map(connectPortComponent), connectBus = _ref1[0], connectPort = _ref1[1];
 
 module.exports = {
+  connectBus: connectBus,
   connectPort: connectPort
 };
 
